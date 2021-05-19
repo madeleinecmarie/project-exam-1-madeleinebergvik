b59129fa24d6e48d4ce1190871396319b953e52e
madeleinecmarie <madeleiinemarie@gmail.com>
2021-05-19 20:51:18 +0200

    slider

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 function sliderFunction() {
-    $('.slide__container').slick({
-        arrows: true,
-        infinite: false,
-        speed: 600,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        dots: true,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
-}
+  $('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+    
+};
 
 async function getSliderContent() {
     try {
@@ -45,22 +45,24 @@ async function getSliderContent() {
         
             if (element.better_featured_image.media_details.sizes.thumbnail.source_url) {
             document.querySelector('.slide__container').innerHTML += `
-            <a href="details_post.html?id=${element.id}">
+          
             <div class="slider__cont">
+            <a href="details_post.html?id=${element.id}">
                 <div class="slider__img slide${element.id}"></div>
                 <h4 class="slider__headline">${element.title.rendered}</h4>
                 <div class="slider__info">
                     <p>${element.excerpt.rendered}</p>
                 </div>
+                </a>
             </div>
-            </a>
+    
             `;
 
             document.querySelector(`.slide${element.id}`).style.backgroundImage = `url(${element.better_featured_image.media_details.sizes.large.source_url})`;
             } else {
             document.querySelector('#slider').innerHTML += `
-                <div class="h2undertitle">   
-                    <h2>${element.title.rendered}</h2>
+                <div class="h4__headline">   
+                    <h4>${element.title.rendered}</h4>
                 </div>
             `;
             }
