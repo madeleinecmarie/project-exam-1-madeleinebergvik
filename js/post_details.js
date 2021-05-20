79ac59653fafae2a980992626bd78581a99e45d0
madeleinecmarie <madeleiinemarie@gmail.com>
2021-05-20 15:02:54 +0200

    hamburger menu

--- js/post_details.js
+++ js/post_details.js
@@ -10,11 +10,8 @@ async function getId(blogId) {
         // console.log(blogId);
         const response = await fetch('https://noroffcors.herokuapp.com/https://travelapi.mcmo.tech/wp-json/wp/v2/posts/' + blogId);
         const jsonResults = await response.json();
-        // const blogArray = jsonResults.data;
-        // console.log(blogArray);
-
-        // document.title = jsonResults.title.rendered;
-
+      
+        document.title = jsonResults.title.rendered;
         document.querySelector('.detail__container').innerHTML = `
         <h1 class="h1__headline__js">${jsonResults.title.rendered}</h1>
         <div class="content__text">${jsonResults.content.rendered}</div>
--- js/script.js
+++ js/script.js
@@ -73,3 +73,22 @@ async function getSliderContent() {
     }
 }
 getSliderContent();
+
+
+// HAMBURGER MENU 
+
+const mainMenu = document.querySelector('.ul__elements');
+const closeMenu = document.querySelector('.close');
+const openMenu = document.querySelector('.open');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
+
+function close() {
+  mainMenu.style.top = '-100%';
+}
