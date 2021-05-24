e7e87d507afd1bb0861aad710418dc562514111b
madeleinecmarie <madeleiinemarie@gmail.com>
2021-05-24 11:26:41 +0200

    media queries

--- js/script.js
+++ js/script.js
@@ -9,17 +9,19 @@ function sliderFunction() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 4,
           infinite: true,
+          arrows: false,
           dots: true
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 700,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 2,
+          arrows: false
         }
       },
       {
